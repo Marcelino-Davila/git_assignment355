79b656371aefabcccf6c6c0ddb1a10cc2f244bc6
Marcelino-Davila <mstephendavils@gmail.com>
2023-11-06 21:01:52 -0500

    This is my program file

---
+++ program.py
@@ -0,0 +1,7 @@
+def checkFraction(x, y):
+    if x >= y:
+        print("the first fraction is greater than or equal to the second fraction")
+        return True
+    else:
+        print("the second fraction is greater than the first fractoin")
+        return False
